4a3723b3e4bd8bf23de40a9e49ba700005abd917
shiv122 <shivtiwari627@gmail.com>
2023-03-17 18:13:22 +0530

    added package buy option

---
+++ database/migrations/2023_03_17_063927_create_user_subscriptions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('package_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('razorpay_order_id')->constrained();
+            $table->json('package_details');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('status')->default('running');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_subscriptions');
+    }
+};
