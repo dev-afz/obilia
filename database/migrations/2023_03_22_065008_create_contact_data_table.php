0de317d83c26b2c55535b32ce0b114c8c742e6fd
shiv122 <37039611+shiv122@users.noreply.github.com>
2023-03-22 12:52:15 +0530

    added contact from

---
+++ database/migrations/2023_03_22_065008_create_contact_data_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_data', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email', 1000);
+            $table->string('phone')->nullable();
+            $table->string('subject', 3000);
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_data');
+    }
+};
